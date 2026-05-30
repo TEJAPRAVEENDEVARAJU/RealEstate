cc807fa91b65e34fe014a7de8acbba1e53e034f4
OBULASETTY TEJA PRAVEEN DEVA RAJU <120026630+TEJAPRAVEENDEVARAJU@users.noreply.github.com>
2026-05-30 16:06:11 +0530

    Update db_config.php

--- Realestate/db_config.php
+++ Realestate/db_config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("sql200.infinityfree.com", "if0_39282857", "G6wDbAohtp4I", "if0_39282857_realestate");
+$conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
   die(json_encode(["message" => "DB Connection failed"]));
 }
